9c88c4ffd45d853419fa40e388ec2dcda1d9490b
tanyichao <tanyichao@foreveross.com>
2016-04-12 10:54:07 +0800

    加入node 模块

--- server.js
+++ server.js
@@ -1,61 +1,33 @@
-var http = require("http"),
-url = require("url"),
-path = require("path"),
-fs = require("fs");
+var express = require('express');
+var app = express();
+var path = require('path');
+var httpProxy = require('http-proxy');
 
-http.createServer(function(req, res) {
-    var pathname = __dirname + url.parse(req.url).pathname;
-    if (path.extname(pathname) == "") {
-        pathname += " / ";
-    }
-    if (pathname.charAt(pathname.length - 1) == " / ") {
-        pathname += "index.html";
-    }
 
-    fs.exists(pathname,
-    function(exists) {
-        if (exists) {
-            switch (path.extname(pathname)) {
-            case".html":
-                res.writeHead(200, {"Content - Type": "text / html"
-                });
-                break;
-            case".js":
-                res.writeHead(200, {"Content - Type": "text / javascript"
-                });
-                break;
-            case".css":
-                res.writeHead(200, {"Content - Type": "text / css"
-                });
-                break;
-            case".gif":
-                res.writeHead(200, {"Content - Type": "image / gif"
-                });
-                break;
-            case".jpg":
-                res.writeHead(200, {"Content - Type": "image / jpeg"
-                });
-                break;
-            case".png":
-                res.writeHead(200, {"Content - Type": "image / png"
-                });
-                break;
-            default:
-                res.writeHead(200, {"Content - Type": "application / octet - stream"
-                });
-            }
+var proxy = new httpProxy.createProxyServer({
+	target: {
+		host: '61.186.243.102',
+		port: 80
+	}
+});
 
-            fs.readFile(pathname,
-            function(err, data) {
-                res.end(data);
-            });
-        } else {
-            res.writeHead(404, {"Content - Type": "text / html"
-            });
-            res.end("404 Not Found");
-        }
-    });
+app.use(function(req, res, next) {
+	if (req.url.match(new RegExp('^\/appcv\/'))) {
+		console.log('proxy to: 61.186.243.102');
+		proxy.web(req, res);
+	} else {
+		next();
+	}
+});
 
-}).listen(80, "127.0.0.1");
+app.use(express.static(path.resolve('.')));
 
-console.log("Server running at http: //127.0.0.1:80/");
+app.get('/', function(req, res) {
+	res.redirect('index.html');
+});
+
+
+app.listen(process.env.PORT || 8001);
+
+console.log('i am up.');
+console.log('listen to 8001');
